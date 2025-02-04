d37d23d538832d93184b14fc5d8fd837ca5d4c6d
Akhilvarma12 <akhilvarma2004@gmail.com>
2025-02-04 20:26:24 +0530

    added MongoDB schema and also added signup page

--- models/user.js
+++ models/user.js
@@ -1,14 +1,51 @@
-const {Schema}=require("mongoose");
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
 
-const UserSchema =new Schema({
-    fullName:{
-        type:String,
-        required:true,
-
+const UserSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/default.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+    this.salt=salt;
+    this.password=hashedPassword
 });
+
+const User = model("user", UserSchema);
+
+module.exports = User;
