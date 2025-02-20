d28dff45e64eea87d78c2249e583a23388329263
Akhilvarma12 <akhilvarma2004@gmail.com>
2025-02-20 16:04:53 +0530

    added the option to upload files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cookieParser=require("cookie-parser")
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
@@ -30,5 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
 
 app.listen(PORT, () => console.log(`Server started at port:${PORT}`));
--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,32 @@
 const { Router } = require("express");
 const router=Router()
+const multer=require("multer");
+const path=require("path");
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve('./public/uploads/'))
+    },
+    filename: function (req, file, cb) {
+        const fileName=`${Date.now()}-${file.originalname}`;
+        cb(null,fileName);
+    }
+  })
+  
+const upload=multer({storage: storage});
+
+router.get('/add-new',(req,res)=>{
+    return res.render('addBlog',{
+        user:req.user,
+    })
+})
+
+router.post('/',upload.single('coverImage'),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    return res.redirect('/')
+})
 
 
 
